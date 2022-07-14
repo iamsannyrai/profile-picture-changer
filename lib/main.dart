532b9cf3ab21a7bd5f91d5f9f5412e0a070a0765
Sanny Rai <sannyrai@sannys-MacBook-Pro.local>
2022-07-14 13:43:28 +0545

    upload image to s3, retrieve image url and preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
+
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'amplifyconfiguration.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,12 +16,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Profile Picture Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Profile Picture'),
     );
   }
 }
@@ -31,12 +39,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? image;
+  bool loading = false;
+  final picker = ImagePicker();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<void> _configureAmplify() async {
+    final auth = AmplifyAuthCognito();
+    final storage = AmplifyStorageS3();
+    await Amplify.addPlugins([storage, auth]);
+    try {
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      debugPrint('Tried to configure Amplify and following error occurred: $e');
+    }
+  }
+
+  Future<void> uploadImageAndSetImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile == null) {
+      return;
+    }
+
+    final key = DateTime.now().toString();
+    final file = File(pickedFile.path);
+    try {
+      final UploadFileResult result = await Amplify.Storage.uploadFile(
+        local: file,
+        key: key,
+        onProgress: (progress) {
+          debugPrint(
+              "${progress.currentBytes}, total: ${progress.totalBytes}, ${progress.getFractionCompleted()}");
+        },
+      );
+      final imageUrl = await getImageUrl(result.key);
+      setState(() {
+        image = imageUrl;
+      });
+    } on StorageException catch (e) {
+      debugPrint('Error uploading image: $e');
+    }
+  }
+
+  Future<String> getImageUrl(String bucketKey) async {
+    try {
+      final result = await Amplify.Storage.getUrl(key: bucketKey);
+      return result.url;
+    } on StorageException catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
   }
 
   @override
@@ -46,23 +103,33 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: GestureDetector(
+          onTap: () async {
+            setState(() {
+              loading = true;
+            });
+            await uploadImageAndSetImage();
+            setState(() {
+              loading = false;
+            });
+          },
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: image != null ? NetworkImage(image!) : null,
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: loading ? const CircularProgressIndicator() : null,
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
