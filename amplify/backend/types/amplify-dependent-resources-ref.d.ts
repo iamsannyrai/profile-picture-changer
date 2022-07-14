b234631e314c671975b53570a72fe3128c7547ae
Sanny Rai <sannyrai@sannys-MacBook-Pro.local>
2022-07-14 13:08:01 +0545

    initialize flutter and amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "imageuploaderf8e370b9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "S3friendlyName": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
